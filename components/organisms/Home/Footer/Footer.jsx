94602f0c884efa1832a04a1653621f8180e70165
Cavan Law <cavan.law@viooh.com>
2021-10-02 15:58:28 +0100

    comment out unused code

--- components/organisms/Home/Footer/Footer.jsx
+++ components/organisms/Home/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import IconButton from '@components/atoms/IconButton';
 
 import linkedIn from '@icons/linkedin.png';
-import instagram from '@icons/instagram.png';
 import github from '@icons/github.png';
-import youtube from '@icons/youtube.png';
+// import instagram from '@icons/instagram.png';
+// import youtube from '@icons/youtube.png';
 
 const Footer = () => (
   <footer className="pt-12 px-4 pb-5 text-white text-center bg-gradient-to-tr from-red-700 via-indigo-700 to-blue-900 opacity-80">
@@ -14,8 +14,8 @@ const Footer = () => (
 
     <div className="flex justify-center gap-5 mb-12">
       <IconButton icon={linkedIn} link="https://www.linkedin.com/in/cavan-l-48682417/" />
-      {/* <IconButton icon={instagram} link="#" /> */}
       <IconButton icon={github} link="https://github.com/cav2008" />
+      {/* <IconButton icon={instagram} link="#" /> */}
       {/* <IconButton icon={youtube} link="#" /> */}
     </div>
 
