42f45f27c72ab0a4d0158525d5b0cc7d51874cdf
Cavan Law <cavan.law@viooh.com>
2021-10-02 18:03:12 +0100

    reduce text size on button component

--- components/atoms/Button/Button.jsx
+++ components/atoms/Button/Button.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Button = ({ text, onClick }) => (
   <button
     type="button"
-    className="px-5 py-2.5 text-lg text-white text-medium leading-tight rounded-lg border-2 border-teal-500 hover:bg-teal-500"
+    className="px-5 py-2.5 text-md text-white text-medium leading-tight rounded-lg border-2 border-teal-500 hover:bg-teal-500"
     onClick={onClick}
   >
     {text}
